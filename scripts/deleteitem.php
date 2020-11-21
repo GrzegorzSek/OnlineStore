a30a004b717529ea75f3c61b98ff2f573d4a518c
GrzegorzSek <241778@student.pwr.edu.pl>
2020-11-21 20:53:28 +0100

    delete-item-from-cart-database-fix

--- scripts/deleteitem.php
+++ scripts/deleteitem.php
@@ -3,7 +3,16 @@
     
     $itemId=$_POST['itemId'];
 
-    $sql = "DELETE FROM cartitem WHERE `id`='$itemId'";
+	$sql = "DELETE FROM cartitem WHERE `id`='$itemId'";
+
+	$productQuantityInCart = "SELECT * FROM cartitem WHERE id='$itemId'";
+    $result = mysqli_query($link, $productQuantityInCart);
+    $fetchQuantity = mysqli_fetch_array($result);
+	$productQuantityInCartValue = $fetchQuantity['product_quantity'];
+	$productId = $fetchQuantity['product_id'];
+
+	$restoreProductQuantity = "UPDATE product SET quantity=quantity+'$productQuantityInCartValue' WHERE id='$productId'";
+	mysqli_query($link, $restoreProductQuantity);
 
 	if (mysqli_query($link, $sql)) {
 		echo json_encode(array("statusCode"=>200));
